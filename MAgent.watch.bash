1b1fcbb66a9dc2798f8e7e7aa766f469a5314659
catallena7 <catallena7@localhost>
2015-10-21 22:30:33 +0900

    xml locataion & bash file modification

--- MAgent.watch.bash
+++ MAgent.watch.bash
@@ -1,25 +1,23 @@
 #!/bin/bash
 
 JAVA_HOME="/opt/jdk1.8.0_51"
-AGENT_HOME="/home/catallena7/workspace-luna-client/Metheus-Agent"
-PS_NAME="MAgent.jar"
-CONF_FILE="${AGENT_HOME}/configure.ini
+AGENT_HOME="/home/catallena7/workspace-luna-client/Agent"
+JAR_NAME="MAgent.jar"
+CONF_FILE="${AGENT_HOME}/configure.xml"
 
-agentPIDs=`ps -ef |grep java |grep jar |grep  ${PS_NAME} |grep -v grep| awk '{print $2}'`
+agentPIDs=`ps -ef |grep java |grep jar |grep  ${JAR_NAME} |grep -v grep| awk '{print $2}'`
 
 if [ "$agentPIDs" ] ; then
-	echo "Agent is already running
+	echo "Agent is already running"
 	echo $agentPIDs
 else
 	cd $AGENT_HOME
 	cwd=`pwd`
 	if [ $cwd == $AGENT_HOME ] && [ -f $CONF_FILE ] ; then
-		${JAVA_HOME}/bin/java -jar ${AGENT_HOME}/MAgent.jar ${CONF_FILE}
+		${JAVA_HOME}/bin/java -jar ${AGENT_HOME}/${JAR_NAME} ${CONF_FILE}
 		echo "RUN Agent at $cwd"
 	else
 		echo "Something is wrong. Please check your directory or configuration file"
-		echo "conf=$CONF_FILE"
-		echo "home=$AGENT_HOME"
 	fi
 fi
 	
